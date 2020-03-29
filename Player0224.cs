8337e98e44714eb903fe38874fc93b54abba0c97
田中聡大 <tanakasodai@tanakasatoshidainoMacBook-Pro.local>
2020-03-29 10:43:25 +0900

    add new file

---
+++ Player0224.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey("up")){
+        transform.position += transform.forward *0.1f;
+    }
+        if(Input.GetKey("right")){
+        transform.Rotate(0,1,0);
+    }
+        if(Input.GetKey("left")){
+        transform.Rotate(0,-1,0);
+    }
+}
+}
